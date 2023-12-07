82776f4b9abb0b37625fedfd3592f0f94360a4fd
dell <dell@DESKTOP-OB7FVAR>
2023-12-07 23:33:38 +0300

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Template1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.panelControl = new System.Windows.Forms.Panel();
+            this.btnForm2 = new System.Windows.Forms.Button();
+            this.btnForm1 = new System.Windows.Forms.Button();
+            this.panelCorner = new System.Windows.Forms.Panel();
+            this.lblScreenTitle = new System.Windows.Forms.Label();
+            this.panelScreens = new System.Windows.Forms.Panel();
+            this.btnForm3 = new System.Windows.Forms.Button();
+            this.lblLineOnTheLeftBtn = new System.Windows.Forms.Label();
+            this.panelControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelControl
+            // 
+            this.panelControl.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelControl.Controls.Add(this.lblLineOnTheLeftBtn);
+            this.panelControl.Controls.Add(this.btnForm3);
+            this.panelControl.Controls.Add(this.btnForm2);
+            this.panelControl.Controls.Add(this.btnForm1);
+            this.panelControl.Controls.Add(this.panelCorner);
+            this.panelControl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelControl.Location = new System.Drawing.Point(0, 0);
+            this.panelControl.Name = "panelControl";
+            this.panelControl.Size = new System.Drawing.Size(179, 486);
+            this.panelControl.TabIndex = 0;
+            // 
+            // btnForm2
+            // 
+            this.btnForm2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnForm2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnForm2.FlatAppearance.BorderSize = 0;
+            this.btnForm2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForm2.Font = new System.Drawing.Font("Microsoft New Tai Lue", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForm2.Location = new System.Drawing.Point(0, 158);
+            this.btnForm2.Name = "btnForm2";
+            this.btnForm2.Size = new System.Drawing.Size(179, 79);
+            this.btnForm2.TabIndex = 1;
+            this.btnForm2.Text = "Button Form2";
+            this.btnForm2.UseVisualStyleBackColor = true;
+            this.btnForm2.Click += new System.EventHandler(this.btnForm2_Click);
+            // 
+            // btnForm1
+            // 
+            this.btnForm1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnForm1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnForm1.FlatAppearance.BorderSize = 0;
+            this.btnForm1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForm1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForm1.Location = new System.Drawing.Point(0, 79);
+            this.btnForm1.Name = "btnForm1";
+            this.btnForm1.Size = new System.Drawing.Size(179, 79);
+            this.btnForm1.TabIndex = 0;
+            this.btnForm1.Text = "Button Form1";
+            this.btnForm1.UseVisualStyleBackColor = true;
+            this.btnForm1.Click += new System.EventHandler(this.btnForm1_Click);
+            // 
+            // panelCorner
+            // 
+            this.panelCorner.BackColor = System.Drawing.Color.LightGray;
+            this.panelCorner.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCorner.Location = new System.Drawing.Point(0, 0);
+            this.panelCorner.Name = "panelCorner";
+            this.panelCorner.Size = new System.Drawing.Size(179, 79);
+            this.panelCorner.TabIndex = 0;
+            // 
+            // lblScreenTitle
+            // 
+            this.lblScreenTitle.BackColor = System.Drawing.Color.Silver;
+            this.lblScreenTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblScreenTitle.Font = new System.Drawing.Font("Microsoft New Tai Lue", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScreenTitle.Location = new System.Drawing.Point(179, 0);
+            this.lblScreenTitle.Name = "lblScreenTitle";
+            this.lblScreenTitle.Size = new System.Drawing.Size(761, 79);
+            this.lblScreenTitle.TabIndex = 2;
+            this.lblScreenTitle.Text = "Title";
+            this.lblScreenTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelScreens
+            // 
+            this.panelScreens.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelScreens.Location = new System.Drawing.Point(179, 79);
+            this.panelScreens.Name = "panelScreens";
+            this.panelScreens.Size = new System.Drawing.Size(761, 407);
+            this.panelScreens.TabIndex = 3;
+            // 
+            // btnForm3
+            // 
+            this.btnForm3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnForm3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnForm3.FlatAppearance.BorderSize = 0;
+            this.btnForm3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForm3.Font = new System.Drawing.Font("Microsoft New Tai Lue", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForm3.Location = new System.Drawing.Point(0, 237);
+            this.btnForm3.Name = "btnForm3";
+            this.btnForm3.Size = new System.Drawing.Size(179, 79);
+            this.btnForm3.TabIndex = 2;
+            this.btnForm3.Text = "Button Form3";
+            this.btnForm3.UseVisualStyleBackColor = true;
+            this.btnForm3.Click += new System.EventHandler(this.btnForm3_Click);
+            // 
+            // lblLineOnTheLeftBtn
+            // 
+            this.lblLineOnTheLeftBtn.BackColor = System.Drawing.Color.Black;
+            this.lblLineOnTheLeftBtn.Location = new System.Drawing.Point(3, 79);
+            this.lblLineOnTheLeftBtn.Name = "lblLineOnTheLeftBtn";
+            this.lblLineOnTheLeftBtn.Size = new System.Drawing.Size(4, 79);
+            this.lblLineOnTheLeftBtn.TabIndex = 3;
+            this.lblLineOnTheLeftBtn.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(940, 486);
+            this.Controls.Add(this.panelScreens);
+            this.Controls.Add(this.lblScreenTitle);
+            this.Controls.Add(this.panelControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(956, 525);
+            this.Name = "frmMain";
+            this.Text = "Template1";
+            this.panelControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelControl;
+        private System.Windows.Forms.Button btnForm1;
+        private System.Windows.Forms.Panel panelCorner;
+        private System.Windows.Forms.Label lblScreenTitle;
+        private System.Windows.Forms.Panel panelScreens;
+        private System.Windows.Forms.Button btnForm2;
+        private System.Windows.Forms.Button btnForm3;
+        private System.Windows.Forms.Label lblLineOnTheLeftBtn;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Template1
+{
+    public partial class frmMain : Form
+    {
+        private void _OpenForm(Form frm)
+        {
+            if (frmPrev != null)
+                frmPrev.Close();// delete prev form to make the application faster
+
+            frm.FormBorderStyle = FormBorderStyle.None;
+            frm.Dock = DockStyle.Fill;
+            frm.TopLevel = false;
+            frm.BringToFront();
+            frmPrev = frm;
+            panelScreens.Controls.Add(frm);
+            frm.Show();
+        }
+        private void _OpenScreen(Form frm, Button btn, Color LightColor, Color DarkColor)
+        {
+            if (_ActiveBtn != null)
+            {
+                _ActiveBtn.ForeColor = Color.Black;
+                _ActiveBtn.BackColor = Color.WhiteSmoke;
+            }
+
+            lblLineOnTheLeftBtn.Visible = true;
+            lblLineOnTheLeftBtn.Location = btn.Location;
+            lblLineOnTheLeftBtn.BackColor = DarkColor;
+
+            btn.BackColor = Color.Gainsboro;
+            btn.ForeColor = DarkColor;
+            lblScreenTitle.Text = frm.Text;
+            lblScreenTitle.BackColor = DarkColor;
+            _OpenForm(frm);
+        }
+        private Button _ActiveBtn = null;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        Form frmPrev = null;
+
+        private void btnForm1_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            _OpenScreen(frm, (Button)sender, Color.Aquamarine, Color.MediumAquamarine);
+            _ActiveBtn = (Button)sender;
+        }
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            _OpenScreen(frm, (Button)sender, Color.IndianRed, Color.Brown);
+            _ActiveBtn = (Button)sender;
+        }
+        private void btnForm3_Click(object sender, EventArgs e)
+        {
+            Form3 frm = new Form3();
+            _OpenScreen(frm, (Button)sender, Color.DarkMagenta, Color.Purple);
+            _ActiveBtn = (Button)sender;
+        }
+    }
+}
